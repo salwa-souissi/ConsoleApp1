0a2fe190cd163557d02c86fa65f4f1af100c5f76
Mahdi Sassi <m.sassi@root.sa>
2018-02-27 13:00:51 +0100

    add class models with property name

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,7 +10,10 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello tunisia");
+            Models model = new Models();
+            model.name = "hello tunisia";
+            Console.WriteLine(model.name);
+            Console.ReadLine();
             /*                           */
 
         }
